a29e62debe24c6661ed535755308f5f39188ebcd
Diego Jimenes <contato.diegojimenes@gmail.com>
2025-06-29 20:44:10 -0300

    fix: resolve storybook css error

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,12 @@
 import type { Preview } from '@storybook/react-vite'
-import '../src/App.css'
+import '../src/index.css'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
